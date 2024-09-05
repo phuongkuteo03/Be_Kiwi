90d392117b765fc5d7fbcd0ea505894b4cc40bf1
Hoang Phuong <phuongvipxs@gmail.com>
2024-09-05 19:51:56 +0700

    first commit

---
+++ src/main/java/com/ikigai/kiwi/entity/Category.java
@@ -0,0 +1,53 @@
+package com.ikigai.kiwi.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+@Data
+@Entity
+@Table(name = "Category")
+public class Category implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer cateId;
+
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameVietNam;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameEnglish;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameFrance;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameDenmark;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameJapan;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameKorea;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameSq;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameArgentina;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameZh;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameEstonia;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameEl;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameIt;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNamePt;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameRusia;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameRs;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameSo;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameThailand;
+    @Column(columnDefinition = "nvarchar(255)")
+    String cateNameUkraine;
+}
---
+++ src/main/java/com/ikigai/kiwi/entity/Story.java
@@ -0,0 +1,62 @@
+package com.ikigai.kiwi.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+@Data
+@Entity
+@Table(name = "Story")
+public class Story implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer storyId;
+
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameVietNam;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameEnglish;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameFrance;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameDenmark;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameJapan;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameKorea;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameSq;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameArgentina;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameZh;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameEstonia;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameEl;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameIt;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNamePt;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameRusia;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameRs;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameSo;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameThailand;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyNameUkraine;
+    @Column(columnDefinition = "nvarchar(255)")
+    String storyAvatarUrl;
+    @Column(columnDefinition = "nvarchar(50)")
+    String storyData;
+    @Column(columnDefinition = "nvarchar(50)")
+    String areaStory;
+
+    @ManyToOne @JoinColumn(name = "cateId")
+    private Category category;
+}
